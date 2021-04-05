24f5f2479b9c462a6ef80d5e03c5447c3f3bfc6b
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-05 19:56:18 +0530

    Implemented User Controller With new and create actions for User Registration Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  
   namespace :admin do
     get 'dashboard', to: 'pages#dashboard'
   end
+
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create', as: 'users'
 end

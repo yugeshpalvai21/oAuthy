3a9eacaf66600e716ef6bb0ef56f9fe90d59166a
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-06 14:28:11 +0530

    implements helper method for display sign in links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def show_auth_links
+    if user_signed_in? 
+      "<h4>Signed In As - #{current_user.username}</h4>" +
+      "#{link_to "Sign Out", sign_out_path}"
+    else 
+      "#{link_to 'Sign Up', sign_up_path}" + 
+      "#{link_to 'Sign In', sign_in_path}"
+     end
+  end
 end

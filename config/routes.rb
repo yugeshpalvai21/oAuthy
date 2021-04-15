779d151044ce1202528560ac672eba70c9a7fa9e
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-15 12:08:29 +0530

    clears routes for devise integration

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
     get 'dashboard', to: 'pages#dashboard'
   end
 
-  get 'sign_up', to: 'users#new'
-  post 'sign_up', to: 'users#create', as: 'users'
+  # get 'sign_up', to: 'users#new'
+  # post 'sign_up', to: 'users#create', as: 'users'
 
-  get 'sign_in', to: 'sessions#new'
-  post 'sign_in', to: 'sessions#create'
-  get 'sign_out', to: 'sessions#destroy'
+  # get 'sign_in', to: 'sessions#new'
+  # post 'sign_in', to: 'sessions#create'
+  # get 'sign_out', to: 'sessions#destroy'
 end

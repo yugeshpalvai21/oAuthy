9ca59a1c55cbcbbf2b93fbb02cec11fab3e3d9ee
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-16 01:12:57 +0530

    implements devise scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'auth', path_names: {sign_in: 'login'},controllers: {registrations: 'users/registrations', omniauth_callbacks: 'users/callbacks'}
   root 'pages#home'
+
+  devise_scope :user do
+    get '/signup', to: 'users/registrations#new'
+  end
   
   namespace :admin do
     get 'dashboard', to: 'pages#dashboard'

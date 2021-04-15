d046690cd918e64220efd8609ea0b856ac918663
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-15 13:06:40 +0530

    Implements Basic Authentication With User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,29 @@
 class ApplicationController < ActionController::Base
-  helper_method :user_signed_in?, :current_user
+  # helper_method :user_signed_in?, :current_user
   
   private
 
-  def user_signed_in?
-    session[:user_id] ? true : false
-  end
+  # def user_signed_in?
+  #   session[:user_id] ? true : false
+  # end
 
-  def current_user
-    if user_signed_in?
-      @user ||= User.find(session[:user_id])
-    else
-      nil
-    end
-  end
+  # def current_user
+  #   if user_signed_in?
+  #     @user ||= User.find(session[:user_id])
+  #   else
+  #     nil
+  #   end
+  # end
 
-  def authenticate_user
-    if user_signed_in?
-      true
-    else
-      redirect_to sign_up_path, notice: 'sign up first'
-    end
-  end
+  # def authenticate_user
+  #   if user_signed_in?
+  #     true
+  #   else
+  #     redirect_to sign_up_path, notice: 'sign up first'
+  #   end
+  # end
 
-  def redirect_signed_in_user
-    redirect_to root_path, notice: 'You cant register while youre signed in' if user_signed_in?
-  end
+  # def redirect_signed_in_user
+  #   redirect_to root_path, notice: 'You cant register while youre signed in' if user_signed_in?
+  # end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def show_auth_links
     if user_signed_in? 
-      "<h4>Signed In As - #{current_user.username}</h4>" +
-      "#{link_to "Sign Out", sign_out_path}"
+      "<h4>Signed In As - #{current_user.email}</h4>" +
+      "#{link_to "Sign Out", destroy_user_session_path, method: :delete}"
     else 
-      "#{link_to 'Sign Up', sign_up_path}" + 
-      "#{link_to 'Sign In', sign_in_path}"
+      "#{link_to 'Sign Up', new_user_registration_path}" + 
+      "#{link_to 'Sign In', new_user_session_path}"
      end
   end
 end

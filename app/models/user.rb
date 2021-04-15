dae36003a84881f56327903beca828767a25492f
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-15 23:02:41 +0530

    Configures OmniAuthable With Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
   #has_secure_password
 
   validates :username, uniqueness: true
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
+      user.confirmed_at = Time.now
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'auth', path_names: {sign_in: 'login'},controllers: {registrations: 'users/registrations'}
+  devise_for :users, path: 'auth', path_names: {sign_in: 'login'},controllers: {registrations: 'users/registrations', omniauth_callbacks: 'users/callbacks'}
   root 'pages#home'
   
   namespace :admin do

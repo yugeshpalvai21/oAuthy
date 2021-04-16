d0a792621e6903c3a743f233c2cd0103287e8e3d
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-16 13:13:29 +0530

    Implementing role based feature using Rolify gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
   #has_secure_password
+
+  after_create :add_default_role
 
   validates :username, uniqueness: true
 
@@ -16,5 +19,9 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
+  end
+
+  def add_default_role
+    self.add_role(:regular)
   end
 end
